bdb1b7eacd2a475b178b0f5364940853a7465406
Trishla Pokharna <trishla.pokharna@gmail.com>
2017-03-24 00:56:48 -0700

    matchList

--- app/src/main/java/com/example/trishlapokharna/scoutingtakethree/MatchList.java
+++ app/src/main/java/com/example/trishlapokharna/scoutingtakethree/MatchList.java
@@ -1,6 +1,10 @@
 package com.example.trishlapokharna.scoutingtakethree;
 
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -18,12 +22,32 @@ public class MatchList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
         View in = inflater.inflate(R.layout.fragment_match_list, container, false); // adds Autonomous tab to input activity
 
-        matchList = (ImageView) in.findViewById(R.id.robopicture);
-        matchList.setImageResource(R.drawable.les);
+      //  matchList = (ImageView) in.findViewById(R.id.match);
+        //matchList.setImageResource(R.drawable.merge2);
 
+        // BitmapFactory.Options options = new BitmapFactory.Options();
+        //options.inScaled = false;
         return in;
 
     }
-    }
 
 
+   /* public Bitmap BITMAP_RESIZER(Bitmap bitmap,int newWidth,int newHeight) {
+        Bitmap scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888);
+
+        float ratioX = newWidth / (float) bitmap.getWidth();
+        float ratioY = newHeight / (float) bitmap.getHeight();
+        float middleX = newWidth / 2.0f;
+        float middleY = newHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bitmap, middleX - bitmap.getWidth() / 2, middleY - bitmap.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+        return scaledBitmap;
+
+    }*/
+}
